9074b6e326873a033d29ad66e7998509187e5bf4
kevin endres <k.m.endres@gmail.com>
2022-09-22 16:43:08 -0500

    Basic prompt loop

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use std::io::{self, Write};
+
 fn main() {
-    println!("Hello, world!");
+    loop {
+        let current_path = std::env::current_dir().expect("Error current_dir");
+        let prefix = current_path.as_path().parent().expect("Error parent dir");
+        let current_dir = current_path.as_path().strip_prefix(prefix).expect("error strip prefix");
+        print!("{}: $ ", current_dir.display());
+        io::stdout().flush().expect("Error flush");
+        let mut buffer = String::new();
+        io::stdin().read_line(&mut buffer).expect("Error couldn't read stdin");
+        if buffer == "exit\n" {
+            break;
+        }
+    }
 }
