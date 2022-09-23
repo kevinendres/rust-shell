74a5c1bf0cd071be10ea8fd1380f7285444a85f8
kevinendres <k.m.endres@gmail.com>
2022-09-23 17:36:15 -0500

    Changed to nix crate for syscalls

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,16 @@
 use std::io::{self, Write};
+use std::ffi::CString;
+use std::ffi::CStr;
+use nix::unistd::ForkResult;
 
 fn main() {
     loop {
         let current_path = std::env::current_dir().expect("Error current_dir");
         let prefix = current_path.as_path().parent().expect("Error parent dir");
-        let current_dir = current_path.as_path().strip_prefix(prefix).expect("error strip prefix");
+        let _current_dir = current_path.as_path().strip_prefix(prefix).expect("error strip prefix");
 
-        print!("{}: $ ", current_dir.display());
+        //print!("{}: $ ", current_dir.display());
+        print!("$ ");
         io::stdout().flush().expect("Error flush");
 
         let mut buffer = String::new();
@@ -16,11 +20,29 @@ fn main() {
             Ok(_) => {}         //bytes read successfully
         }
 
-        let input: Vec<&str> = buffer.split(' ').collect();
+        let input: Vec<CString> = buffer
+            .split_whitespace()
+            .map(|s| CString::new(s).expect("Error CString"))
+            .collect();
+        let path = &input[0];
+        let args = if input.len() > 1 {
+                        &input[..]
+                    }
+                    else {
+                        &[] as &[CString]
+                    };
 
-        let mut command = std::process::Command::new(input[0]);
-        command.args(&input[1..]);
+        let fork_res;
+        unsafe { fork_res = nix::unistd::fork().expect("Error fork"); };
 
-        command.output().expect("failed to execute command");
+        match fork_res {
+            ForkResult::Child => {
+                let _execve_result = nix::unistd::execve(path, args, &[] as &[&CStr]);
+            },
+            ForkResult::Parent {
+                child: c } => {
+                let _wait_status = nix::sys::wait::waitpid(c, None);
+            },
+        }
     }
 }
