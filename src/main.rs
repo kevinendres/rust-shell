e898d9ceeb632ed68b858d45d263466bbdefeaf6
kevin endres <k.m.endres@gmail.com>
2022-09-22 20:35:25 -0500

    Added command spawning

--- src/main.rs
+++ src/main.rs
@@ -5,12 +5,22 @@ fn main() {
         let current_path = std::env::current_dir().expect("Error current_dir");
         let prefix = current_path.as_path().parent().expect("Error parent dir");
         let current_dir = current_path.as_path().strip_prefix(prefix).expect("error strip prefix");
+
         print!("{}: $ ", current_dir.display());
         io::stdout().flush().expect("Error flush");
+
         let mut buffer = String::new();
-        io::stdin().read_line(&mut buffer).expect("Error couldn't read stdin");
-        if buffer == "exit\n" {
-            break;
+        match io::stdin().read_line(&mut buffer) {
+            Err(err) => eprintln!("{}", err),
+            Ok(0) => break,     //EOF, end shell
+            Ok(_) => {}         //bytes read successfully
         }
+
+        let input: Vec<&str> = buffer.split(' ').collect();
+
+        let mut command = std::process::Command::new(input[0]);
+        command.args(&input[1..]);
+
+        command.output().expect("failed to execute command");
     }
 }
