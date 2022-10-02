7357ce628029fce90ff0ae67afdf22b7cdb90e63
kevin endres <k.m.endres@gmail.com>
2022-10-02 17:19:27 -0500

    Refactored reading/executing into Command

--- src/command/mod.rs
+++ src/command/mod.rs
@@ -1,4 +1,69 @@
+use std::io;
+use std::ffi::CString;
+use nix::unistd::ForkResult;
+use std::path::PathBuf;
+use std::str::FromStr;
+
 pub struct Command {
-    bin: String,
-    args: Vec<String>,
+    bin: CString,
+    args: Vec<CString>,
+}
+
+impl Command {
+    pub fn read_from_stdin<'a>() -> Command {
+        let mut buffer = String::new();
+        match io::stdin().read_line(&mut buffer) {
+            Err(err) => eprintln!("{}", err),
+            Ok(0) => std::process::exit(0),     //EOF, end shell
+            Ok(_) => {}         //bytes read successfully
+        }
+
+        let mut input: Vec<&str> = buffer.split_whitespace().collect();
+        let bin = match input[0] {
+            "cd" => {
+                CString::new("cd").expect("CString failure")
+            },
+            "pwd" => {
+                CString::new("pwd").expect("CString failure")
+
+            }
+            "/bin/pwd" => {
+                CString::new("pwd").expect("CString failure")
+            }
+
+            bin => CString::new(PathBuf::from_str(bin)
+                                        .expect("Error pathbuf")
+                                        .file_name()
+                                        .expect("Error Filename")
+                                        .to_str()
+                                        .expect("OsStr to Str"))
+                                        .expect("CString to filename"),
+        };
+        let args = input
+                        .iter()
+                        .map(|s| CString::new((s).as_bytes()).expect("Error CString"))
+                        .collect::<Vec<CString>>();
+
+        Command { bin, args }
+    }
+
+    pub fn execute(&self) -> i32 {
+        let fork_res;
+        unsafe { fork_res = nix::unistd::fork().expect("Error fork"); };
+
+        let result = match fork_res {
+            ForkResult::Child => {
+                nix::unistd::execvp(&self.bin, self.args.as_slice()).expect("Exec fail");
+                1
+            },
+            ForkResult::Parent {
+                child: c } => {
+                match nix::sys::wait::waitpid(c, None).ok() {
+                    Some(nix::sys::wait::WaitStatus::Exited(pid, code)) => code,
+                                                                      _ => 1,
+                }
+            },
+        };
+        result
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,83 +1,27 @@
-mod builtins;
 mod command;
 
-use std::io::{self, Write};
-use std::ffi::CString;
-use nix::unistd::ForkResult;
-use std::path::{PathBuf, Path};
-use std::str::FromStr;
+use std::env;
+use std::io::Write;
+use command::Command;
+
+fn print_prompt(code: i32) {
+    let path = env::current_dir().expect("Error! evn::current_dir failed");
+    let current_dir = path.file_name().unwrap().to_str();
+
+    match code {
+        0 => print!("\x1b[92m{}: ?\x1b[0m ", current_dir.unwrap_or(">>>")),
+        _ => print!("\x1b[91m{}: ?\x1b[0m ", current_dir.unwrap_or(">>>")),
+    }
+    std::io::stdout().flush().expect("Error flush");
+}
 
 fn main() {
+    let mut last_result = 0;
     loop {
-        let current_path = std::env::current_dir().expect("Error current_dir");
-        let parent_dir = if current_path != PathBuf::from("/") {
-            current_path.as_path().parent().expect("Error parent dir")
-        }
-        else {
-            &current_path
-        };
-        let current_dir = current_path.as_path().strip_prefix(parent_dir).expect("error strip prefix");
 
-        print!("{}: $ ", current_dir.display());
-        io::stdout().flush().expect("Error flush");
+        print_prompt(last_result);
+        let command = Command::read_from_stdin();
+        last_result = command.execute();
 
-        let mut buffer = String::new();
-        match io::stdin().read_line(&mut buffer) {
-            Err(err) => eprintln!("{}", err),
-            Ok(0) => break,     //EOF, end shell
-            Ok(_) => {}         //bytes read successfully
-        }
-
-        let mut input: Vec<&str> = buffer.split_whitespace().collect();
-        let filename = match input[0] {
-            "cd" => {
-                // input[0] = "chdir";
-                // input[1] = match input[1] {
-                //     ".." => parent_dir.to_str().expect("Error parent_dir to str"),
-                //     unchanged => unchanged,
-                // };
-                // println!("{:?}", input);
-                // CString::new("chdir").expect("Error cd to CString")
-                builtins::cd::cd(&input);
-                continue;
-                
-            },
-
-            "/bin/pwd" => {
-                builtins::pwd::pwd();
-                continue;
-            }
-
-            bin => CString::new(PathBuf::from_str(bin)
-                                        .expect("Error pathbuf")
-                                        .file_name()
-                                        .expect("Error Filename")
-                                        .to_str()
-                                        .expect("OsStr to Str"))
-                                        .expect("CString to filename"),
-        };
-        let cstring_input = input
-                            .iter()
-                            .map(|s| CString::new((s).as_bytes()).expect("Error CString"))
-                            .collect::<Vec<CString>>();
-        let args = if input.len() > 1 {
-                        cstring_input.as_slice()
-                    }
-                    else {
-                        &[] as &[CString]
-                    };
-
-        let fork_res;
-        unsafe { fork_res = nix::unistd::fork().expect("Error fork"); };
-
-        match fork_res {
-            ForkResult::Child => {
-                let _execve_result = nix::unistd::execvp(&filename, args);
-            },
-            ForkResult::Parent {
-                child: c } => {
-                let _wait_status = nix::sys::wait::waitpid(c, None);
-            },
-        }
     }
 }
